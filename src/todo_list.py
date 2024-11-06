1cb625e9d08f629594aeb370e89c8a12166cae48
Jin Li <l960728@outlook.com>
2024-11-06 22:41:37 +0800

    update

--- src/todo_list.py
+++ src/todo_list.py
@@ -1,3 +1,10 @@
+from todo import Todo
+
+
 class TodoList:
     def __init__(self) -> None:
+        self.todos: list[Todo] = []
+        pass
+
+    def add_todo(self, todo: str) -> None:
         pass
